571f8ca92a54384292e764fb6633b1d1b6f2eced
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2023-12-22 23:26:59 +0530

    Added the weather-icon functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiURL + `&q=${city}` + `&appid=${apiKey}`);
@@ -14,11 +15,29 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = `${data.main.temp}°C`;
     document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
     document.querySelector(".wind").innerHTML = `${data.wind.speed} km/hr`;
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+        weatherIcon.src = "images/snow.png";
+    }
 }
 
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value)
 });
-// checkWeather(delhi);
+
 
